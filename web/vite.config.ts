ca86ec2a28fd20f92c8627043a21bb1a4470b7c7
david-why <david_why@outlook.com>
2025-03-03 21:05:45 +0800

    this works right

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,11 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const BASE_URL = process.env.BASE_URL || '/';
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: BASE_URL,
+  base: '',
   plugins: [
     vue(),
     vueDevTools(),
