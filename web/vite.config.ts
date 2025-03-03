717adc0acbfe57e39caa40539e1c9dc62d003fbb
david-why <david_why@outlook.com>
2025-03-03 21:02:24 +0800

    add env file

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,8 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const BASE_URL = process.env.BASE_URL || '/';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: BASE_URL,
   plugins: [
     vue(),
     vueDevTools(),
