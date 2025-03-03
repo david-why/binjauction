1fc2039f959272688e1fed2139aea352f14ee6b6
david-why <david_why@outlook.com>
2025-03-03 20:43:18 +0800

    initial commit

---
+++ web/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar">
+    <img src="@/assets/logo.svg" style="height: 1em" />
+    <!-- <span style="height: 1em"> -->
+    <span><strong>Silent Auction</strong></span>
+    <!-- </span> -->
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  padding: 1em;
+  background-color: var(--color-background-mute);
+  gap: 1em;
+}
+</style>
