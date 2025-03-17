f8f29a6c7867f6e0d8bc715e0235647056f741c2
david-why <david_why@outlook.com>
2025-03-17 22:36:26 +0800

    fix: add that missing line

--- functions/api/config.ts
+++ functions/api/config.ts
@@ -1,4 +1,5 @@
 export const onRequestGet: AuctionPagesFunction = async (context) => {
   const result = await context.env.DB.prepare('SELECT value FROM config').first<{ value: string }>();
   const config = JSON.parse(result?.value || '{}');
+  return Response.json(config)
 }
